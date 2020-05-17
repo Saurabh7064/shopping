2469c7112651a0c14bcabaa405d90040caa54e87
Saurabh <emosrb@gmail.com>
2020-05-17 22:48:51 +0530

    121. Adding ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,22 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
  
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test','https://i.ytimg.com/vi/3Vf2uPmP2gU/maxresdefault.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test','https://i.ytimg.com/vi/3Vf2uPmP2gU/maxresdefault.jpg')
+        new Recipe('A Test Recipe', 'This is simply a testt','https://i.ytimg.com/vi/3Vf2uPmP2gU/maxresdefault.jpg',
+        [
+          new Ingredient('Meat',1),
+          new Ingredient('French Fries', 20)
+        ]),
+        new Recipe('Another Test Recipe', 'This is simply a testt','https://i.ytimg.com/vi/3Vf2uPmP2gU/maxresdefault.jpg',
+        [
+          new Ingredient('Buns', 2),
+          new Ingredient('Meat', 1)
+        ])
       ];     
 
       getRecipes() {
