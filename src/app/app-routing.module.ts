17a5bf259df8b40133322ac950ef9ad847bfdc21
Saurabh <emosrb@gmail.com>
2020-05-26 13:41:31 +0530

    283. Resolving Data Before Loading using Resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,16 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.component';
+import { RecipeResolverService } from './recipes/recipe-resolver.service';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
     { path: 'recipes', component: RecipesComponent, children:[
       {path:'', component: RecipesStartComponent},
       {path: 'new', component: RecipesEditComponent},
-      {path: ':id', component: RecipeDetailComponent},
-      {path: ':id/edit', component: RecipesEditComponent} 
-    ]},
+      {path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService]},
+      {path: ':id/edit', component: RecipesEditComponent, resolve: [RecipeResolverService]} 
+    ], resolve: [RecipeResolverService]},
     { path: 'shopping-list', component: ShoppingListComponent},
 ]
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,7 +13,6 @@ export class HeaderComponent {
   }
 
   onFetchData() {
-    this.dataStorageService.fetchRecipes();
-
+    this.dataStorageService.fetchRecipes().subscribe();
   }
 }
