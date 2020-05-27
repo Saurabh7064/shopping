b0af180b258134b32e0d33753ba3a02eff3acedc
Saurabh <emosrb@gmail.com>
2020-05-27 09:17:30 +0530

    297. Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
@@ -25,19 +25,7 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-            ).pipe(
-                catchError(errorRes => {
-                    let errorMessage = 'An unknown error occurred!';
-                    if(!errorRes.error || !errorRes.error.error) {
-                        return throwError(errorMessage);
-                    }
-                    switch (errorRes.error.error.message) {
-                        case 'EMAIL_EXISTS':
-                        errorMessage = 'This email already exists'
-                    }   
-                    return throwError(errorMessage);
-                })
-            );
+            ).pipe(catchError(this.handleError));
     }
 
     login(email: string, password: string) {
@@ -48,7 +36,26 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        );
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred!';
+                    if(!errorRes.error || !errorRes.error.error) {
+                        return throwError(errorMessage);
+                    }
+                    switch (errorRes.error.error.message) {
+                        case 'EMAIL_EXISTS':
+                          errorMessage = 'This email exists already';
+                          break;
+                        case 'EMAIL_NOT_FOUND':
+                          errorMessage = 'This email does not exist.';
+                          break;
+                        case 'INVALID_PASSWORD':
+                          errorMessage = 'This password is not correct.';
+                          break;
+                      }
+                    return throwError(errorMessage);
     }
 
 
