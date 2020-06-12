dbe468a6226e6a69bd980adc53ed29833512169d
Saurabh <emosrb@gmail.com>
2020-06-12 23:33:55 +0530

    361-363  Auth Reducer and Actions

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,9 @@ import { throwError, Subject, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
-
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 export interface AuthResponseData {
     kind: string;
@@ -23,8 +25,12 @@ export class AuthService {
     user = new BehaviorSubject<User>(null);
     private tokenExpirationTimer: any;
 
-    constructor(private http: HttpClient, private router: Router) { }
-
+    constructor(
+      private http: HttpClient,
+      private router: Router,
+      private store: Store<fromApp.AppState>
+    ) {}
+    
     signup(email: string, password: string) {
        return this.http.post<AuthResponseData>(
             'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='+environment.firebaseAPIKey
@@ -83,7 +89,15 @@ export class AuthService {
       );
   
       if (loadedUser.token) {
-        this.user.next(loadedUser);
+        // this.user.next(loadedUser);
+        this.store.dispatch(
+          new AuthActions.Login({
+            email: loadedUser.email,
+            userId: loadedUser.id,
+            token: loadedUser.token,
+            expirationDate: new Date(userData._tokenExpirationDate)
+          })
+        );
         const expirationDuration =
           new Date(userData._tokenExpirationDate).getTime() -
           new Date().getTime();
@@ -92,7 +106,8 @@ export class AuthService {
     }
 
     logout() {
-      this.user.next(null);
+      // this.user.next(null);
+      this.store.dispatch(new AuthActions.Logout());
       this.router.navigate(['/auth']);
       localStorage.removeItem('userData');
       if (this.tokenExpirationTimer) {
@@ -108,17 +123,25 @@ export class AuthService {
     }
 
     private handleAuthentication(
-        email: string,
-        userId: string,
-        token: string,
-        expiresIn: number
-      ) {
-        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-        const user = new User(email, userId, token, expirationDate);
-        this.user.next(user);
-        this.autoLogout(expiresIn * 1000);
-        localStorage.setItem('userData', JSON.stringify(user));
-      }
+      email: string,
+      userId: string,
+      token: string,
+      expiresIn: number
+    ) {
+      const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+      // this.user.next(user);
+      const user = new User(email, userId, token, expirationDate);
+      this.store.dispatch(
+        new AuthActions.Login({
+          email: email,
+          userId: userId,
+          token: token,
+          expirationDate: expirationDate
+        })
+      );
+      this.autoLogout(expiresIn * 1000);
+      localStorage.setItem('userData', JSON.stringify(user));
+    }
 
     
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import * as fromApp from '../store/app.reducer';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -9,18 +12,23 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit, OnDestroy{
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
-
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
+  ) {}
   private userSub: Subscription;
   isAuthenticated = false;
 
   ngOnInit() {
-    this.userSub = this.authService.user.subscribe(user => {
-      this.isAuthenticated = !!user;
-      console.log(!user);
-      console.log(!!user);
-      console.log(user);
-    });
+    this.userSub = this.store
+      .select('auth')
+      .pipe(map(authState => authState.user))
+      .subscribe(user => {
+        this.isAuthenticated = !!user;
+        console.log(!user);
+        console.log(!!user);
+      });
   }
   onSaveData() {
     this.dataStorageService.storeRecipe();
