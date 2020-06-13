9eacbe98439d6356df306a9a1f92dc5e9d72ec5a
Saurabh <emosrb@gmail.com>
2020-06-13 10:16:04 +0530

    364 updated constant values

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,7 +1,9 @@
 import { Action } from '@ngrx/store';
 
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
+export const LOGIN_START = '[Auth] Login Start';
+export const LOGIN = '[Auth] Login';
+export const LOGIN_FAIL = '[Auth] Login Fail';
+export const LOGOUT = '[Auth] Logout';
 
 export class Login implements Action {
   readonly type = LOGIN;
