163f27aa2fd1d40a7211cc725afeea496885c485
Saurabh <emosrb@gmail.com>
2020-06-14 07:33:54 +0530

    389-402 Not working relook but project is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { isPlatformBrowser } from '@angular/common';
 
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
@@ -13,11 +14,14 @@ import { LoggingService } from './logging.service.module';
 export class AppComponent implements OnInit {
   constructor(
     private store: Store<fromApp.AppState>,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    @Inject(PLATFORM_ID) private platformId
   ) {}
 
   ngOnInit() {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.loggingService.printLog('Hello from AppComponent ngOnInit');
   }
 }
